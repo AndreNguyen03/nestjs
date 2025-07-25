8cfc073b27566e536016dea16a36564719aa6207
Ngoc Anh <ngocanh030110@gmail.com>
2025-07-25 16:58:33 +0700

    17 File Uploads to s3+cloudfront

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { DataResponseInterceptor } from './common/interceptors/data-response/data-response.interceptor';
-
+import { ConfigService } from '@nestjs/config';
+import { config } from 'aws-sdk'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
@@ -15,18 +16,28 @@ async function bootstrap() {
     }
   }))
 
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('NestJS API')
     .setDescription('API documentation for the NestJS Blog application, base URL on http://localhost:3000')
     .setTermsOfService('http://localhost:3000/term-of-service')
-    .setLicense('MIT License','https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt')
+    .setLicense('MIT License', 'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt')
     .addServer('http://localhost:3000', 'Development server')
     .setVersion('1.0')
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
 
   SwaggerModule.setup('api', app, document);
+
+  // setup aws sdk used uploading thefiles to aws s3 bucket
+  const configService = app.get(ConfigService);
+  config.update({
+    credentials: {
+      accessKeyId: configService.get('appConfig.awsAccessKeyId')!,
+      secretAccessKey: configService.get('app.awsSecretAccessKey')!,
+    },
+    region: configService.get('appConfig.awsRegion')
+  })
 
   // enable cors;
   app.enableCors();
